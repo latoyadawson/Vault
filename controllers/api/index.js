7c56014023c8c1f2ab74714f63bf55826a4d4729
latoyadawson <78005063+latoyadawson@users.noreply.github.com>
2021-05-21 00:23:49 -0700

    starter readMe

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,5 +4,6 @@ const userRoutes = require('./user-routes');
 const budgetRoutes = require('./budget-routes');
 
 router.use('/users', userRoutes);
+router.use('/budget', budgetRoutes);
 
 module.exports = router;
