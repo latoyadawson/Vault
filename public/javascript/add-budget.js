66a98312cdbe827a50b8b8ee5ca4bed01500e0c1
latoyadawson <78005063+latoyadawson@users.noreply.github.com>
2021-05-23 17:24:06 -0700

    intial file

--- public/javascript/add-budget.js
+++ public/javascript/add-budget.js
@@ -16,11 +16,12 @@ async function addBudgetFormHandler(event) {
     });
   
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
-  };
-    
-  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+};
+
+//replace tbd with class or id 
+document.querySelector('.TBD').addEventListener('submit', addBudgetFormHandler);
   
